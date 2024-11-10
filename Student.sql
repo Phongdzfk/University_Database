a63e9910d1ff284b0a2cf4cb82023d75799633ed
Tungtom2004 <114893594+Tungtom2004@users.noreply.github.com>
2024-11-11 00:01:31 +0700

    Update Student.sql

---
+++ Student.sql
@@ -0,0 +1,12 @@
+CREATE TABLE student (
+  StudentID varchar(55) NOT NULL PRIMARY KEY,
+  firstName varchar(55) NOT NULL,
+  middleName varchar(55) NOT NULL,
+  lastName varchar(55) NOT NULL,
+  Date_of_Birth varchar(55) NOT NULL,
+  Address varchar(55) NOT NULL,
+  Gender varchar(55) NOT NULL,
+  phoneNumber varchar(55) NOT NULL,
+  EmailAddress varchar(55) NOT NULL,
+  Emergency_Contact varchar(55) NOT NULL
+)
