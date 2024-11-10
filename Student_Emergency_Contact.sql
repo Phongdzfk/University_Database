c588e13230087726b6ba4a7145adf79248b07c4a
Tungtom2004 <114893594+Tungtom2004@users.noreply.github.com>
2024-11-10 23:56:39 +0700

    Update Student_Emergency_Contact.sql

--- Student_Emergency_Contact.sql
+++ Student_Emergency_Contact.sql
@@ -2,4 +2,6 @@ CREATE TABLE university_domitory.Student_Emegency_Contact(
 	 Emergency_Contact VARCHAR(55) NOT NULL,
      StudentID VARCHAR(55) NOT NULL,
      FOREIGN KEY (StudentID) REFERENCES student(StudentID)
-)
+);
+INSERT INTO Student_Emergency_Contact
+VALUES("113","HS001"),
