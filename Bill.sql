91fceef297215a08317e1cd0f8a824820cc1f584
Tungtom2004 <114893594+Tungtom2004@users.noreply.github.com>
2024-11-11 00:21:58 +0700

    Update Bill.sql

--- Bill.sql
+++ Bill.sql
@@ -1,14 +1,14 @@
-CREATE TABLE `contract` (
-  `ContractID` varchar(55) NOT NULL,
-  `Contract_Start_Date` varchar(55) NOT NULL,
-  `Contract_End_Date` varchar(55) NOT NULL,
-  `Contract_Status` varchar(55) NOT NULL,
-  `Penalty_for_Violation` varchar(55) NOT NULL,
-  `PersonalID_O` varchar(55) NOT NULL,
-  `StudentID` varchar(55) NOT NULL,
-  PRIMARY KEY (`ContractID`),
-  KEY `PersonalID_O` (`PersonalID_O`),
-  KEY `StudentID` (`StudentID`),
-  CONSTRAINT `contract_ibfk_1` FOREIGN KEY (`PersonalID_O`) REFERENCES `office_staff` (`PersonalID_O`),
-  CONSTRAINT `contract_ibfk_2` FOREIGN KEY (`StudentID`) REFERENCES `student` (`StudentID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+CREATE TABLE bill (
+  Bill_ID varchar(55) NOT NULL,
+  Bill_Type varchar(55) NOT NULL,
+  Amount_Due int NOT NULL,
+  Due_Date varchar(55) NOT NULL,
+  Payment_Status varchar(55) NOT NULL,
+  StudentID varchar(55) NOT NULL,
+  PersonalID_O varchar(55) NOT NULL,
+  PRIMARY KEY (Bill_ID),
+  KEY StudentID (StudentID),
+  KEY PersonalID_O (PersonalID_O),
+  FOREIGN KEY (StudentID) REFERENCES student (StudentID),
+  FOREIGN KEY (PersonalID_O) REFERENCES office_staff (PersonalID_O)
+) 
