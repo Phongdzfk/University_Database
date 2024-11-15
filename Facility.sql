3edf1c986c99708fd2ccea472fc9df39c873443f
Phongdzfk <117740087+Phongdzfk@users.noreply.github.com>
2024-11-16 00:25:42 +0700

    Update Facility.sql

--- Facility.sql
+++ Facility.sql
@@ -1,7 +1,7 @@
 CREATE TABLE facility (
+  Room_ID varchar(55) NOT NULL,
   itemName varchar(55) NOT NULL,
   Overall_Quality varchar(55) NOT NULL,
-  Room_ID varchar(55) NOT NULL,
-  PRIMARY KEY (itemName),
-  FOREIGN KEY (Room_ID) REFERENCES room (Room_ID)
-) 
+  PRIMARY KEY (Room_ID, itemName),  -- Composite key
+  FOREIGN KEY (Room_ID) REFERENCES room (Room_ID)  -- Foreign key reference to the room table
+);
