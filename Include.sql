30ced842d8605a4c2da37577be6232aa64faac26
Phongdzfk <117740087+Phongdzfk@users.noreply.github.com>
2024-11-16 00:33:20 +0700

    Update Include.sql

--- Include.sql
+++ Include.sql
@@ -1,7 +1,8 @@
 CREATE TABLE include (
   Room_ID varchar(55) NOT NULL,
-  Item_Name varchar(55) NOT NULL,
+  itemName varchar(55) NOT NULL,
   Quantity int NOT NULL,
-  FOREIGN KEY (Room_ID) REFERENCES room (Room_ID),
-  FOREIGN KEY (Item_Name) REFERENCES facility (itemName)
-)
+  PRIMARY KEY (Room_ID, itemName),  -- Composite primary key
+  FOREIGN KEY (Room_ID) REFERENCES room (Room_ID),  -- Foreign key to room table
+  FOREIGN KEY (Room_ID, itemName) REFERENCES facility (Room_ID, itemName)  -- Foreign key to facility table
+);
