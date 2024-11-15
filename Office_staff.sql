506fc2e0ed3a1308e2b87b1f4eaf7af09897cb49
Tungtom2004 <114893594+Tungtom2004@users.noreply.github.com>
2024-11-15 22:05:07 +0700

    Update Office_staff.sql

--- Office_staff.sql
+++ Office_staff.sql
@@ -3,4 +3,11 @@ CREATE TABLE office_staff (
   PersonalID_O varchar(55) NOT NULL,
   PRIMARY KEY (PersonalID_O),
   FOREIGN KEY (PersonalID_O) REFERENCES dormitory_staff (PersonalID)
-) 
+);
+INSERT INTO Office_Staff VALUES
+('O01','000007'),
+('O02','000008'),
+('O03','000009'),
+('O04','000010'),
+('O05','000011'),
+('O06','000012'); 
