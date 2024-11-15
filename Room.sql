22dce57533c47d9e7a9e2b3293a3df7a2f1d808b
Tungtom2004 <114893594+Tungtom2004@users.noreply.github.com>
2024-11-15 23:46:11 +0700

    Update data

--- Room.sql
+++ Room.sql
@@ -3,7 +3,7 @@ CREATE TABLE room (
   number_of_Students int NOT NULL,
   Condition_of_Room varchar(55) NOT NULL,
   Capacity int NOT NULL,
-  Gender int NOT NULL,
+  Gender VARCHAR(55) NOT NULL,
   Dormitory_ID varchar(55) NOT NULL,
   PRIMARY KEY (Room_ID),
   FOREIGN KEY (Dormitory_ID) REFERENCES Dormitory (Dormitory_ID)
