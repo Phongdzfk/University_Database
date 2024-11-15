bae311086e8287407b9529375bde3d4f3673a02f
Tungtom2004 <114893594+Tungtom2004@users.noreply.github.com>
2024-11-15 22:12:15 +0700

    Update Dormitory_Manager.sql

--- Dormitory_Manager.sql
+++ Dormitory_Manager.sql
@@ -3,4 +3,8 @@ CREATE TABLE dormitory_manager (
   ManagerID varchar(55) NOT NULL,
   PRIMARY KEY (PersonalID_DM),
   FOREIGN KEY (PersonalID_DM) REFERENCES dormitory_staff (PersonalID)
-) 
+);
+INSERT INTO dormitory_manager VALUES
+('000004','M01'),
+('000005','M02'),
+('000006','M03')
