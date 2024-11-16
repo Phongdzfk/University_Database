6c81232e3e4de3dc2e2dfd3191c44c57e7828ba1
Phongdzfk <117740087+Phongdzfk@users.noreply.github.com>
2024-11-16 23:51:31 +0700

    update query

--- Dormitory.sql
+++ Dormitory.sql
@@ -16,11 +16,13 @@ INSERT INTO dormitory (Dormitory_ID, Location, Number_of_Floors, Number_of_Rooms
 ('C', 'Phía Bắc Học Viện', 2, 7, 'Ổn định', '000003');
 
  -- Query 1:
-SELECT Dormitory_ID, Location, Number_of_Rooms from dormitory
-WHERE Number_of_Rooms = (SELECT MAX(Number_of_Rooms) from dormitory);
+SELECT SUM(Number_of_Rooms) AS Total_Rooms
+FROM dormitory;
 
 -- Query 2:
 SELECT PersonalID,firstName,lastName,Salary,Email,Address,State,Dormitory_Assigned,staffType from dormitory_staff
 JOIN dormitory
 ON PersonalID = dormitory.PersonalID_G
 WHERE dormitory.Dormitory_ID = 'A';
+
+-- 
