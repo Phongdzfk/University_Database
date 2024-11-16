64baa503e42fb7b0fbc7d43012c30af8bf6b2858
Phongdzfk <117740087+Phongdzfk@users.noreply.github.com>
2024-11-17 02:53:35 +0700

    update query

--- Complicated_Query.sql
+++ Complicated_Query.sql
@@ -1,3 +1,4 @@
+--Query 1
 WITH BillCounts AS (
     SELECT 
         StudentID,
@@ -40,3 +41,21 @@ GROUP BY
     bc.Unpaid_Amount,
     bc.NumberOfBills
 ORDER BY s.StudentID;
+
+-- Query 2
+SELECT DISTINCT 
+    ds.PersonalID, 
+    ds.Dormitory_Assigned AS staffDormitory,
+    GROUP_CONCAT(DISTINCT dsp.phoneNumber) AS staffPhoneNumbers,
+    st.StudentID, 
+    CONCAT(st.firstname,' ',st.middleName,' ',st.lastName) AS studentName,
+    GROUP_CONCAT(DISTINCT sec.Emergency_Contact) AS student_Emegency_Contact
+FROM dormitory_staff ds
+JOIN dormitory d ON ds.Dormitory_Assigned = d.Dormitory_ID
+JOIN live l ON l.PersonalID_DM = ds.PersonalID 
+JOIN student st ON st.StudentID = l.StudentID
+LEFT JOIN student_emegency_contact sec ON st.StudentID = sec.StudentID
+LEFT JOIN dormitory_staff_phonenumber dsp ON ds.PersonalID = dsp.PersonalID
+WHERE ds.staffType = 'Manager'
+GROUP BY ds.PersonalID, st.StudentID
+ORDER BY ds.PersonalID, st.StudentID;
